37e708c4bed0215d91b46b2e71f90ee054a6adc7
Omar Oulji <omaroulj@gmail.com>
2021-09-13 16:34:22 +0200

    "Changes to NumericalIntegration"
    "Made into a packeage and reestructures into a classes file"

--- dependencies/NumericalIntegration.py
+++ dependencies/NumericalIntegration.py
@@ -1,96 +1,105 @@
 import numpy as np 
+from abc import abstractclassmethod, ABC
 
 #Numerical methods for integration.
+#TO DO: Change to docstring
+class Integrator(ABC):
 
-def EulerODE(t,y0,fun):
+    @abstractclassmethod
+    def integrate(t, y0:np.array,fun,*args):
+        pass
 
-#  Euler method integration. 
-# -------------------------------------------
-#  * t is the vector (array) containing the independent variable values where the ODE is to be evaluated.
-#  * y0 is the initial value for the problem. 
-#  * fun is the f(t,y) function obtained when the EDO is expressed as y' = f(t,y)
+class EulerIntegrator(Integrator):
 
-    y = np.zeros(len(t)) #y values for the ODE. 
-    y[0] = y0 #Initial condition is the first value of the array.
+    def integrate(t, y0:np.array ,fun):
 
-    #Euler integration:
+        #  Euler method integration. 
+        # -------------------------------------------
+        #  * t is the vector (array) containing the independent variable values where the ODE is to be evaluated.
+        #  * y0 is the initial value for the problem. 
+        #  * fun is the f(t,y) function obtained when the EDO is expressed as y' = f(t,y)
 
-    for i in range(len(t)-1):
-        y[i+1] = y[i] + (t[i+1]-t[i])*fun(t[i],y[i])
-    return y
+            y = np.zeros(len(t)) #y values for the ODE. 
+            y[0] = y0 #Initial condition is the first value of the array.
 
-#Same as previus, but it takes a 2D vector as the initial conditions
+            #Euler integration:
 
-def EulerODE2D(t, y0:np.array ,fun):
+            for i in range(len(t)-1):
+                y[i+1] = y[i] + (t[i+1]-t[i])*fun(t[i],y[i])
+            return y
 
-    y = np.zeros([len(t),2]) #y values for the ODE. 
-    y[0] = y0[0]
-    y[1] = y0[1] #Initial condition is the first value of the array.
+class Euler2DIntegrator(Integrator):
 
-    for i in range(len(t)-1):
-        y[i+1,0] = y[i,0] + (t[i+1]-t[i])*fun(t[i],y[i,0])
-        y[i+1,1] = y[i,1] + (t[i+1]-t[i])*fun(t[i],y[i,1])
-    return y
+    def integrate(t, y0:np.array ,fun):
+        y = np.zeros([len(t),2]) #y values for the ODE. 
+        y[0] = y0[0]
+        y[1] = y0[1] #Initial condition is the first value of the array.
 
-# RK4 integration (classic Runge-Kutta method) for 1st order ODE.
-# -------------------------------------------
-#  * t is the vector (array) containing the independent variable values where the ODE is to be evaluated.
-#  * y0 is the initial value for the problem. 
-#  * fun is the f(t,y) function obtained when the EDO is expressed as y' = f(t,y)
+        for i in range(len(t)-1):
+            y[i+1,0] = y[i,0] + (t[i+1]-t[i])*fun(t[i],y[i,0])
+            y[i+1,1] = y[i,1] + (t[i+1]-t[i])*fun(t[i],y[i,1])
+        return y
 
-def RK4(t,y0,fun):
+class RK4Integrator(Integrator):
+    
+    def integrate(t,y0,fun):
 
-    y = np.zeros(len(t))
-    y[0] = y0
+        y = np.zeros(len(t))
+        y[0] = y0
 
-    h = t[1]- t[0]
-        
-    for i in range(len(t)-1):
+        h = t[1]- t[0]
+            
+        for i in range(len(t)-1):
 
-        k1 = fun(t[i],y[i])
-        k2 = fun(t[i]+h/2, y[i] + h*(k1/2))
-        k3 = fun(t[i]+h/2, y[i] + h*(k2/2))
-        k4 = fun(t[i]+h , y[i] + h*k3)
+            k1 = fun(t[i],y[i])
+            k2 = fun(t[i]+h/2, y[i] + h*(k1/2))
+            k3 = fun(t[i]+h/2, y[i] + h*(k2/2))
+            k4 = fun(t[i]+h , y[i] + h*k3)
 
-        y[i+1] = y[i] + (1/6)*h*(k1+2*k2+2*k3+k4)
+            y[i+1] = y[i] + (1/6)*h*(k1+2*k2+2*k3+k4)
 
-    return y
+        return y
+
+class RK4_2ndOrder(Integrator):
 
 #RK4 integration (classic Runge-Kutta method) for 2nd order ODE.
 # -------------------------------------------
 #  * t is the vector (array) containing the independent variable values where the ODE is to be evaluated.
 #  * y0 and z0 are the initial values for the problem. 
 #  * f and g are the resulting functions when separating the 2nd order ODE in a system of two first order ODEs
-
-def RK4_2ndOrder(t,y0,z0,f,g):
-
-    y = np.zeros(len(t))
-    y[0] = y0
-    z = np.zeros(len(t))
-    z[0] = z0
-
-    h = h = t[1]- t[0]
-
-    for i in range(len(t)-1):
     
-        k1 = f(t[i], y[i], z[i])
-        l1 = g(t[i], y[i], z[i])
+    def integrate(t,y0,z0,f,g):
+        y = np.zeros(len(t))
+        y[0] = y0
+        z = np.zeros(len(t))
+        z[0] = z0
 
-        k2 = f(t[i]+h/2, y[i] + h*(k1/2), z[i] + h*(l1/2))
-        l2 = g(t[i]+h/2, y[i] + h*(k1/2), z[i] + h*(l1/2))
+        h = h = t[1]- t[0]
 
-        k3 = f(t[i]+h/2, y[i] + h*(k2/2), z[i] + h*(l2/2))
-        l3 = g(t[i]+h/2, y[i] + h*(k2/2), z[i] + h*(l2/2))
+        for i in range(len(t)-1):
+        
+            k1 = f(t[i], y[i], z[i])
+            l1 = g(t[i], y[i], z[i])
 
-        k4 = f(t[i]+h , y[i] + h*k3, z[i] + h*l3)
-        l4 = g(t[i]+h , y[i] + h*k3, z[i] + h*l3)
+            k2 = f(t[i]+h/2, y[i] + h*(k1/2), z[i] + h*(l1/2))
+            l2 = g(t[i]+h/2, y[i] + h*(k1/2), z[i] + h*(l1/2))
 
-        y[i+1] = y[i] + (1/6)*h*(k1+2*k2+2*k3+k4)
-        z[i+1] = z[i] + (1/6)*h*(l1+2*l2+2*l3+l4)
+            k3 = f(t[i]+h/2, y[i] + h*(k2/2), z[i] + h*(l2/2))
+            l3 = g(t[i]+h/2, y[i] + h*(k2/2), z[i] + h*(l2/2))
 
-    results = np.array([[y],[z]])
+            k4 = f(t[i]+h , y[i] + h*k3, z[i] + h*l3)
+            l4 = g(t[i]+h , y[i] + h*k3, z[i] + h*l3)
 
-    return results
+            y[i+1] = y[i] + (1/6)*h*(k1+2*k2+2*k3+k4)
+            z[i+1] = z[i] + (1/6)*h*(l1+2*l2+2*l3+l4)
+
+        results = np.array([[y],[z]])
+
+        return results
+
+
+
+    
 
 
 
