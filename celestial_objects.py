6196b7c1557800b8a2fba3ff11df71a940e5ec15
Omar Oulji <omaroulj@gmail.com>
2021-09-14 21:41:43 +0200

    Makes sure vectors are np arrays

--- celestial_objects.py
+++ celestial_objects.py
@@ -1,26 +1,37 @@
 import numpy as np
 
 
-
 class CelestialObject:
     def __init__(
         self,
-        initial_position: np.array,
         name: str,
         mass: float,
-        initial_velocity: np.array,
+        initial_acceleration,
+        initial_velocity,
+        initial_position,
+        
+
     ) -> None:
 
-        self.mass = mass
-        self.position = initial_position
-        self.inital_velocity = initial_velocity
-        self.name = name
-
-    def update_position(self, new_position: np.array) -> None:
-        self.position = new_position
+        self._mass = mass
+        self._position = np.array(initial_position)
+        self._velocity = np.array(initial_velocity)
+        self._acceleration = np.array(initial_acceleration)
+        self._name = name
     
     def get_mass(self) -> float:
-        return self.mass
+        return self._mass
     
     def get_position(self) -> np.array:
-        return self.position
+        return self._position
+    def get_velocity(self) -> np.array:
+        return self._velocity
+    def get_acceleration(self) -> np.array:
+        return self._acceleration
+    
+    def update_position(self, new_position) -> None:
+        self._position = np.array(new_position)
+    def update_velocity(self, new_velocity) -> None:
+        self._velocity = np.array(new_velocity)
+    def update_acceleration(self, new_acceleration) -> None:
+        self._acceleration= np.array(new_acceleration)
